2cebfac7ba2f228cc329e7d1f7400fbba4dca0bc
albisnaps25-ai <albisnaps25@gmail.com>
2025-09-09 11:55:01 +0200

    Delete src_index.js

--- src_index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './Qwen_jsx_20250909_nf3o7lofb.jsx';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
